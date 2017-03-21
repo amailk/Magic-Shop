0cef8a6817a9f9e06719187fceae3fed1881a84e
Ama Liyanage <liyaniama@gmail.com>
2017-03-20 21:19:50 -0400

    add magic item component

---
+++ src/components/MagicItem.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+export const MagicItem = () =>
+{
+  return (
+    <li>
+      <h3>Title</h3>
+      <img />
+      <p>Description</p>
+      <h4>$2.00</h4>
+      <button>Buy</button>
+    </li>
+  )
+}
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export {MagicItem} from './MagicItem'
