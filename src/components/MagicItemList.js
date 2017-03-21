7e28c13167706897b7f4b7dc5a11c67aec50d6aa
Ama Liyanage <liyaniama@gmail.com>
2017-03-20 22:30:00 -0400

    create MagicItemList component

---
+++ src/components/MagicItemList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {MagicItem} from './MagicItem'
+
+export const MagicItemList = (props) =>
+{
+  return (
+    <ul>
+      {props.items.map(item =>
+        <MagicItem key={item.id} {...item}/>
+      )}
+    </ul>
+  )
+}
