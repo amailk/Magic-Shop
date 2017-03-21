bf67ad10c2acc0db7f56dc0a9dde51e1e782997d
Ama Liyanage <liyaniama@gmail.com>
2017-03-20 22:30:29 -0400

    Fix the image url

--- src/components/MagicItem.js
+++ src/components/MagicItem.js
@@ -6,7 +6,7 @@ export const MagicItem = (props) =>
   return (
     <li>
       <h3>{props.name}</h3>
-      <img src="images/{props.image}" />
+      <img src={"images/" + props.image} />
       <p>{props.description}</p>
       <h4>${props.price}</h4>
       <button>Buy</button>
