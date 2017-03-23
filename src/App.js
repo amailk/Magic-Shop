a84aad0da8557b591d0bae9ef412e6025de85b20
Ama Liyanage <liyaniama@gmail.com>
2017-03-22 20:32:22 -0400

    add routing to the app

--- src/App.js
+++ src/App.js
@@ -2,21 +2,48 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {MagicItemList} from './components'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 var items = require("./../db.json").items;
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Magic Shop</h2>
-        </div>
-        <MagicItemList items={items}/>
-      </div>
-    );
-  }
-}
+const BasicExample = () => (
+  <Router>
+    <div>
+      // navigation for testing
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/cart">Cart</Link></li>
+        <li><Link to="/checkout">Checkout</Link></li>
+      </ul>
 
-export default App;
+      <Route exact path="/" component={Home}/>
+      <Route path="/cart" component={Cart}/>
+      <Route path="/checkout" component={Checkout}/>
+    </div>
+  </Router>
+)
+
+const Cart = () => (
+  <div>
+    <h2>Cart</h2>
+  </div>
+)
+
+const Checkout = () => (
+  <div>
+    <h2>Checkout</h2>
+  </div>
+)
+
+
+const Home = () => (
+  <div className="App">
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>Magic Shop</h2>
+    </div>
+    <MagicItemList items={items}/>
+  </div>
+)
+
+export default BasicExample;
