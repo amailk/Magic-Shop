2a2e6b360096b6f6e63a8400bb012960caa486e3
Ama Liyanage <liyaniama@gmail.com>
2017-04-13 17:28:05 -0400

    remove link to checkout page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ const App = () => (
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/cart">Cart</Link></li>
-        <li><Link to="/checkout">Checkout</Link></li>
       </ul>
 
       <Route exact path="/" component={Home}/>
